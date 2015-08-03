7ac7f821ef36fd06de219d5e7942c4cb9aeac217
saoft <saoft@foxmial.com>
2015-08-03 16:16:46 +0800

    test

--- src/main/java/org/saoft/bbs/dao/ReplyRepository.java
+++ src/main/java/org/saoft/bbs/dao/ReplyRepository.java
@@ -2,10 +2,12 @@ package org.saoft.bbs.dao;
 
 import org.saoft.bbs.entities.Reply;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 /**
  * Created by saoft on 15/8/3.
  */
+@Repository
 public interface ReplyRepository extends JpaRepository<Reply, Long> {
 
 
